b8cd7a6ff4922a0497afd544358b8c7c3dc5486d
sodo2398a <solomonmarshal206@yahoo.com>
2022-06-04 08:41:18 -0500

    commit

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+
+function initmap(){
+    map = new google.maps.map(document.getElementById("map"),{ center: 
+    {lat: 51.500729, lng: -0.124625},
+    zoom:8,});
+}
